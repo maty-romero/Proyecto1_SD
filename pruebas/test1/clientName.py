a4737f4f112ee1dbedd2d3ca53880ff565fae554
Hans <ich.arbeite.rd@gmail.com>
2025-08-21 21:59:49 -0300

    fix test1 - get palabras clientes

---
+++ pruebas/test1/clientName.py
@@ -0,0 +1,13 @@
+import Pyro5.api
+
+servidor = Pyro5.api.Proxy("PYRONAME:ServidorPalabras@10.210.92.214:9090")
+
+while True:
+    palabra = input("Escribe una palabra o ingrese 'salir': ").strip()
+    if palabra.lower() == "salir":
+        break
+    resultado = servidor.enviar_palabra(palabra)
+    if resultado:
+        print("Palabras de otros clientes:\n" + resultado)
+    else:
+        print("Todavía no hay respuestas de otros clientes.")
---
+++ pruebas/test1/serverName.py
@@ -0,0 +1,35 @@
+import Pyro5.api
+
+@Pyro5.api.expose
+class Servidor:
+    def __init__(self):
+        self.respuestas = {}  # {ip_cliente: palabra}
+
+    def enviar_palabra(self, palabra):
+        # Obtener IP del cliente que hace la llamada
+        client_ip = Pyro5.api.current_context.client.sock.getpeername()[0]
+
+        # Guardar la palabra usando la IP como clave
+        self.respuestas[client_ip] = palabra
+        
+        # Devolver las palabras de los demás clientes
+        print(f"Desde Server: Client: {client_ip} | Envio: {palabra} | Count Dict: {len(self.respuestas)}")
+        all_words_str = "\n".join(f"{ip}: {pal}" for ip, pal in self.respuestas.items())
+        
+        return all_words_str
+
+def main():
+    # Crear daemon en la IP accesible desde la red
+    daemon = Pyro5.api.Daemon(host="10.210.92.214")
+    ns = Pyro5.api.locate_ns(host="10.210.92.214", port=9090)
+
+    # Registrar el objeto
+    uri = daemon.register(Servidor())
+    ns.register("ServidorPalabras", uri)
+
+    print("Servidor listo")
+    daemon.requestLoop()
+
+
+if __name__ == "__main__":
+    main()
