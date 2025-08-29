728d50c526cce4622b0a294709b9271a574cf34f
Hans <ich.arbeite.rd@gmail.com>
2025-08-29 00:59:27 -0300

    [refactor] Partial Uso de AbstractGUI y logica general para test

---
+++ desarrollo/v1/GestorPartida.py
@@ -0,0 +1,76 @@
+import json
+from Partida import Partida
+from AbstractGUI import AbstractGUI
+import Pyro5.api
+
+# funciones auxiliares 
+
+def dict_to_json(info: dict) -> str:
+    try:
+        return json.dumps(info, indent=4, ensure_ascii=False)
+    except TypeError as e:
+        raise ValueError(f"No se pudo convertir a JSON: {e}")
+
+
+@Pyro5.api.expose
+class GestorPartida:
+    def __init__(self, gui: AbstractGUI):
+        self.partida = Partida() # jugadores, categorias y nro ronda
+        self.gui = gui
+        gui.show_message("[Servidor Lógico] Objeto Gestor Partida Inicializado.")
+
+    # PENDIENTE - INICIAR_PARTIDA / COMENZAR_RONDA_1
+    def confirmar_jugador(self): # args: nickname ?? 
+        inicio_partida: bool = self.partida.confirmar_jugador_partida()
+        if(inicio_partida):
+            # INICIAR_PARTIDA / JUGAR_RONDA
+
+            # Simulacion para 1 jugador 
+            #jugador = self.partida.get_jugador_partida(nickname)
+            mensaje = self.partida.get_info_ronda()
+            mensaje_json = dict_to_json(mensaje)
+            return mensaje_json
+            #self.enviar_info_ronda()
+
+
+    # Pendiente - DEBERIA SER UN BROADCASTING 
+    def enviar_info_ronda(self, nick_name: str, mensaje: str):
+        self.gui.show_message("*** Server envia mennsaje a Cliente ***")
+        jugador_remoto = self.partida.get_jugador_partida(nickname)
+        if(jugador_remoto is None):
+            self.gui.show_error(f"El Jugador {nickname} no esta en la partida!")
+        
+        jugador_remoto.recibir_info_ronda(mensaje) 
+
+    # PENDIENTE- Patron strategy o command para mandar info ?? 
+
+    def enviar_info_sala(self, nickname: str, mensaje: str):
+        self.gui.show_message("*** Server envia mennsaje a Cliente ***")
+        jugador_remoto = self.partida.get_jugador_partida(nickname)
+        if(jugador_remoto is None):
+            self.gui.show_error(f"El Jugador {nickname} no esta en la partida!")
+
+        jugador_remoto.recibir_info_sala(mensaje) 
+
+    # "Equivalente a => registrar_cliente"
+    def unirse_a_sala(self, nickname: str, nombre_logico: str):
+        proxy_cliente = Pyro5.api.Proxy(f"PYRONAME:{nombre_logico}")
+        self.gui.show_message(f"Registrando jugador: {nickname}, con nombre_logico: {nombre_logico}")
+
+        self.partida.agregar_jugador_partida(nickname, proxy_cliente) # se agrega objeto remoto
+
+        # devolver json info para la sala
+        info_sala_json = dict_to_json(self.partida.get_info_sala())
+
+        # prueba - borrar consola cliente e imprimir json info sala
+        self.enviar_info_sala(nickname, info_sala_json)
+        
+
+    def CheckNickNameIsUnique(self, nickname: str):
+        is_not_string = not isinstance(nickname, str)
+        if (nickname == "" or is_not_string):
+            return "Error: NickName vacio o no es cadena!"
+
+        formated_nickname = nickname.lower().strip(' ')
+        is_unique: bool = self.partida.is_nickname_disponible(formated_nickname)
+        return is_unique
