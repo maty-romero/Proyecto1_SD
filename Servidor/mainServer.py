95dac111ddd0c48ec93dc67b9a68a33d521ee100
Elias <eliasmagallanes2001@gmail.com>
2025-09-27 12:13:53 -0300

    timers

--- Servidor/mainServer.py
+++ Servidor/mainServer.py
@@ -1,7 +1,9 @@
+import time
 import traceback
 import Pyro5.server
 from Pyro5 import errors
 from Cliente.Utils.ComunicationHelper import ComunicationHelper
+from Cliente.Utils.ConsoleLogger import ConsoleLogger
 from Servidor.Aplicacion.NodoServidor import NodoServidor
 from Servidor.Aplicacion.ServidorNombres import ServidorNombres
 from Servidor.Comunicacion.Dispacher import Dispatcher
@@ -11,29 +13,39 @@ from Servidor.Dominio.ServicioJuego import ServicioJuego
     por ejemplo que al momento del registro el nodo incremente un contador en el nameserver,
     pero si se cae, esto puede que no sea tan util.
     """
+
+def medir_tiempo (t1,t2):
+    return t2-t1
+
+
 if __name__ == "__main__":
     
+    inicio_time = time.time()
+    logger_aux=ConsoleLogger(name="LoggerMain", level="INFO")
     #el nameServer existe, y se imprimio en este metodo su info
     ns=ServidorNombres.verificar_nameserver()
 
     if ns:
         try:
             ip_servidor = ComunicationHelper.obtener_ip_local()
+            logger_aux.warning(f"Tiempo actual desde el inicio (nodo+ip): {medir_tiempo(inicio_time,time.time())}")
             nodoPrincipal = NodoServidor(1,None,True)
+            logger_aux.warning(f"Tiempo actual desde el inicio (nodo+ip): {medir_tiempo(inicio_time,time.time())}")  
             Gestor_Singleton = nodoPrincipal.ServicioJuego
             daemon = Pyro5.server.Daemon(host=ip_servidor)
-
+            logger_aux.warning(f"Tiempo actual desde el inicio (daemon+gestor): {medir_tiempo(inicio_time,time.time())}")            
             uri = ComunicationHelper.registrar_objeto_en_ns(
                 Gestor_Singleton,
                 "gestor.partida",
                 daemon,
                 ns
                 )
+            logger_aux.warning(f"Tiempo actual desde el inicio(uri): {medir_tiempo(inicio_time,time.time())}")  
             print("Se inicio el Gestor de juego con los siguientes Datos")
             print(f"URI:  {uri}")
             print(f"daemon:  {daemon}")
             daemon.requestLoop()
-
+            logger_aux.warning(f"Tiempo final: {medir_tiempo(inicio_time,time.time())}")  
         except errors.NamingError:
             print("Servidor de nombres no encontrado")
         except errors.CommunicationError:
